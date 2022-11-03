2cae641d199226952709af1fd3a6132252bb1926
NishanthKuppusamy <nishanthnb1218@gmail.com>
2022-11-03 23:05:22 +0530

    implemented the project

---
+++ StudentManagemengt1/src/main/java/com/abcengineeringcollege/StudentManagemengt1/StudentManagemengt1Application.java
@@ -0,0 +1,13 @@
+package com.abcengineeringcollege.StudentManagemengt1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagemengt1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagemengt1Application.class, args);
+	}
+
+}
---
+++ StudentManagemengt1/src/test/java/com/abcengineeringcollege/StudentManagemengt1/StudentManagemengt1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abcengineeringcollege.StudentManagemengt1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagemengt1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
